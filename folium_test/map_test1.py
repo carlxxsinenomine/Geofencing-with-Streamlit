6b4ce80bde1679f84583a631319d3859dcae062a
est_calexx <10johannesmunoz@gmail.com>
2025-10-21 14:31:12 +0800

    ADDED: the Draw functionality where user can draw shapes such as Circle, Polygons and shits

--- folium_test/map_test1.py
+++ folium_test/map_test1.py
@@ -5,11 +5,19 @@ import streamlit_folium
 import streamlit_folium as st_folium
 from folium import JsCode
 from folium.plugins import Realtime
+from folium.plugins import Draw
 
 
 m = folium.Map(location=[0, 0], zoom_start=12)
+draw = Draw(export=True).add_to(m)
+
+st.set_page_config(layout="wide")
 
 rt = folium.plugins.LocateControl(auto_start=True).add_to(m)
-st.write(rt.to_dict())
 
-st_data = st_folium.st_folium(m, width=725)
+map_col, output_col = st.columns(2)
+
+with map_col:
+    st_data = st_folium.st_folium(m, width=725)
+with output_col:
+    st.write(st_data)
