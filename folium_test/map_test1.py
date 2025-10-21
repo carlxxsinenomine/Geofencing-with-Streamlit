149875d2d2aabc4c87d6fd6ea3cd5e68cc35ae88
est_calexx <10johannesmunoz@gmail.com>
2025-10-21 22:04:53 +0800

    REMOVED: Polylines and Circlemarker feature

--- folium_test/map_test1.py
+++ folium_test/map_test1.py
@@ -1,4 +1,5 @@
 import folium
+import shapely
 import requests
 import streamlit as st
 import streamlit_folium
@@ -7,16 +8,26 @@ from folium import JsCode
 from folium.plugins import Realtime
 from folium.plugins import Draw
 
-
-m = folium.Map(location=[0, 0], zoom_start=12)
-draw = Draw(export=True).add_to(m)
-
+# config for app layout on web
 st.set_page_config(layout="wide")
 
+m = folium.Map(zoom_start=12)
+
+# Draw plugin for drawing shapes on map layer
+# set polyline, and circlemarker to false since nde naman need
+Draw(
+    export=False,
+    draw_options={
+        'polyline': False,
+        'circlemarker': False,
+    }
+).add_to(m)
+
+# for auto location detection
 rt = folium.plugins.LocateControl(auto_start=True).add_to(m)
 
+# layout for widgets in col
 map_col, output_col = st.columns(2)
-
 with map_col:
     st_data = st_folium.st_folium(m, width=725)
 with output_col:
