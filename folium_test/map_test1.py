461c6640b2af79a2034e648c955ce66d5967bab4
est_calexx <10johannesmunoz@gmail.com>
2025-10-23 20:23:21 +0800

    ADDED: title bar and dunno i forgor

--- folium_test/map_test1.py
+++ folium_test/map_test1.py
@@ -11,6 +11,8 @@ from folium.plugins import Draw
 # config for app layout on web
 st.set_page_config(layout="wide")
 
+st.title("Geofence Feature")
+
 # Initialize session state for storing named shapes
 # Everytime na nag iinteract ang user sa web naga-automatic rerun ang streamlit
 # st.session_state jan naka store ung previous state ng app
@@ -28,15 +30,18 @@ def get_color_shape(shape_name):
     lowered_shape_name = shape_name.lower()
     # for now return color strings maya nalang ung actual colors
     if 'safe area' in lowered_shape_name:
-        return 'blue' # blue for sage area
+        return '#647FBC' # blue for sage area
     elif 'high risk area' in lowered_shape_name:
-        return 'red' # Red for high risk
+        return '#F75270' # Red for high risk
     else:
-        return 'green' # Green for evacuation centers etc.
+        return '#A7E399' # Green for evacuation centers etc.
 
 # So here's the plan, all states of the app are stored in the session state
 # meaning if i want to access every property and attribute i can access it through the session state
 # the plan? wala putangina
+# here's the plan, whenever the user has drawn shapes on the map,
+# access its features from the 'last_active_drawing'
+
 
 # Draw plugin for drawing shapes on map layer
 # set polyline, and circlemarker to false since nde naman need
@@ -56,7 +61,7 @@ rt = folium.plugins.LocateControl(auto_start=True).add_to(m)
 map_col, output_col = st.columns(2)
 
 with map_col:
-    st_data = st_folium.st_folium(m, width=725, key="map_reset")
+    st_data = st_folium.st_folium(m, width=725, key="map")
 
 with output_col:
     all_drawings = st_data.get('all_drawings')
@@ -124,3 +129,5 @@ with output_col:
     # Show raw all_drawings for debugging
     with st.expander("Raw All Drawings Data"):
         st.write(all_drawings)
+    with st.expander("Last Active Drawing"):
+        st.write(st_data.get('last_active_drawing'))
