ff064f28d7b9dd32766cb7ac9e1c59668946fd9e
est_calexx <10johannesmunoz@gmail.com>
2025-11-08 16:21:01 +0800

    ADDED: basic config for streamlit and mongodb connection

--- db/Database_Manager.py
+++ db/Database_Manager.py
@@ -3,16 +3,32 @@
 # https://www.mongodb.com/docs/languages/python/pymongo-driver/current/get-started/
 # https://docs.streamlit.io/develop/tutorials/databases/mongodb
 
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
+# Basic Mongodb config with streamlit
+# https://docs.streamlit.io/develop/tutorials/databases/mongodb
+import streamlit as st
+import pymongo
+
+@st.cache_resource
+def init_connection():
+    return pymongo.MongoClient(st.secrets["mongo"])
 
 
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
+client = init_connection()
 
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
+# Pull data from the collection.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def get_data():
+    db = client.sample_mflix
+
+    # You need to specify which collection to query
+    items = db.movies.find().limit(10)
+    return list(items)
+
+
+items = get_data()
+
+# Display the data
+st.write(f"Found {len(items)} documents")
+for item in items:
+    st.write(f"Movie: {item.get('title', 'No title')}")
